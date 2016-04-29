7d84f4654f29c8ba28c191c8ab32a69b0271f8dc
Jan-Christian Nikles <jan.nikles@meetrics.com>
2016-04-29 15:37:26 +0200

    initial draft

---
+++ jquery.serializeJson.js
@@ -0,0 +1,53 @@
+
+/**
+ * jQuery plugin to serialize all inputs decending from an
+ * element as JSON
+ * @author Jan-Christian Nikles
+ */
+$.fn.serializeJson = function() {
+  var inputs, select, textAreas, result, date, momentDate, $input, name;
+
+  inputs = this.find("input");
+  selects = this.find("select");
+  textAreas = this.find("textarea");
+  result = {};
+
+  inputs.each(function (index, input) {
+    $input = $(input);
+    name = $input.attr("name");
+
+    switch(input.type) {
+      case 'password':
+      case 'text':
+        result[name] = $input.val();
+        break;
+      case 'date':
+        date = new Date($input.val());
+        momentDate = moment($input.val());
+        if (momentDate.isValid()) {
+          result[name] = date;
+        }
+        break;
+      case 'checkbox':
+        result[name] = $input.is(":checked");
+        break;
+      case 'number':
+        result[name] = parseInt($input.val());
+    }
+  });
+
+  selects.each(function (index, select) {
+    $input = $(select);
+    name = $input.attr("name");
+
+    result[name] = $input.val();
+  });
+
+  textAreas.each(function (index, area) {
+    $input = $(area);
+    name = $input.attr("name");
+    result[name] = $input.val();
+  });
+
+  return result;
+};
